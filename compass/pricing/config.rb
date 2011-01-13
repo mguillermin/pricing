9a592e6771df4737c4839bb85cab6d2d564292b1
Matthieu Guillermin <matthieu.guillermin@gmail.com>
2011-01-13 22:42:45 +0100

    Playing with compass for the project css

---
+++ compass/pricing/config.rb
@@ -0,0 +1,11 @@
+# Require any additional compass plugins here.
+require "lemonade"
+
+# Set this to the root of your project when deployed:
+http_path = "http://localhost:9000/public/"
+css_dir = "../../public/stylesheets"
+sass_dir = "src"
+images_dir = "../../public/images"
+javascripts_dir = "../../public/javascripts"
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
