ce218936527b17b03dd52f659eab0cd22824d5c5
Matthieu Guillermin <matthieu.guillermin@gmail.com>
2011-01-16 20:50:11 +0100

    Completing unit tests

--- test/BasicTest.java
+++ test/BasicTest.java
@@ -30,5 +30,15 @@ public class BasicTest extends UnitTest {
         assertEquals((Double)15D, ch1.getAmount());
 
         assertEquals((Double)7900D, ch1.getPrice());
+        
+        Profile p1cp = ch1.getProfileByPosition(1L);
+        assertNotNull(p1cp);
+        assertEquals("CP", p1cp.title);
+        assertEquals((Double)700D, p1cp.rate);
+        
+        p1cp.rate = 650D;
+        p1cp.save();
+        
+        assertEquals((Double)7650D, ch1.getPrice());
     }
 }
